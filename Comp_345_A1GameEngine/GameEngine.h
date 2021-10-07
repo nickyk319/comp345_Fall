5238649482d0f01ff7d13f0968430071ef4ef561
Fangwei Huang <84631340+yui-wong-0726@users.noreply.github.com>
2021-10-07 15:53:21 -0400

    Add files via upload

---
+++ Comp_345_A1GameEngine/GameEngine.h
@@ -0,0 +1,113 @@
+﻿#pragma once
+
+#include "Cards.h"
+//#include "Map.h"
+#include "Order.h"
+#include "OrdersList.h"
+
+
+enum class GameState {
+	start, map_loaded, map_validated, players_added, assign_reinforcement, issue_orders, execute_orders, win
+};
+
+class GameEngine {
+
+private:
+	GameState state;
+	//Map* map;
+	OrdersList* ol;
+	Deck* deck;
+	// TODO: player list
+	// std::vector<Player*> playerList;
+	int numOfPlayers;
+
+public:
+	GameEngine();
+
+	string stateToString() {
+		switch (state) {
+		case GameState::start:
+			return "start";
+		case GameState::map_loaded:
+			return "map_loaded";
+		case GameState::map_validated:
+			return "map_validated";
+		case GameState::players_added:
+			return "players_added";
+		case GameState::assign_reinforcement:
+			return "assign_reinforcement";
+		case GameState::issue_orders:
+			return "issue_orders";
+		case GameState::execute_orders:
+			return "execute_orders";
+		case GameState::win:
+			return "win";
+		}
+	}
+
+	GameState getState() {
+		return state;
+	}
+
+	void setState(GameState s) {
+		state = s;
+	}
+
+	/* the following functions represents the transition functions(edges) */
+
+	// startup phase
+	void loadMap();
+	// 1. ask for player input for a map file path
+	// 2. create a maploader instance
+	// 3. pass the file path to maploader
+	// 4. load map(MapLoader.readMap()), and make your game engine store the map that was loaded
+	// 5. if load fails(file path not found or other error), repeat 1-4
+	// 6. if load succeed, print message to console.
+
+	void validateMap();
+	// 1. use the Map.validate() function to validate map. 
+	// 2. if map not valid, return false.
+	// if valid, print message to console.
+
+
+	void addPlayer();
+	// 1. ask for user input for number of players
+	// 2. use loop to create players and put them into a playerlist
+	// players added successfully, print message. 
+
+
+	void assignCountries();
+	// Assign every country in the map to all players in round robin fashion.
+	// ??startupphase()
+	// ?????Territory?pid
+
+
+
+// need a function for assign reinforcement?
+
+
+	void issueOrder();
+	// call the issue order function of each player
+	// issue order frunction should return an order pointer.
+	// store the returned order into order list (ol).
+
+
+	void endIssueOrders();
+
+	void executeOrder();
+	// loop over order list (ol), and call the validate() and execute() function of each order
+	// once an order is done executing, delete it from the order list. 
+
+
+	void endExecuteOrders();
+
+	void checkWinner();
+	// tell the player if a winner has be decided.
+
+	void replay();
+
+	void end() {
+		exit(0);
+	}
+
+};
