6ed652b525c879a2abf2354c872ddfe70a9c92bb
Hansenkk <55026853+Hansenkk@users.noreply.github.com>
2021-12-03 08:36:09 -0500

    Add files via upload

--- PlayerStrategies.cpp
+++ PlayerStrategies.cpp
@@ -1,11 +1,14 @@
 #include "PlayerStrategies.h"
-
+#include <cstdlib>
 PlayerStrategy::PlayerStrategy() : player(nullptr) {}
 PlayerStrategy::~PlayerStrategy() {}
 void PlayerStrategy::setPlayer(Player* player) {
     this->player = player;
 }
 
+static int getRandom(int a, int b) {
+    return (rand() % (b - a + 1)) + a;
+}
 
 
 
@@ -88,82 +91,10 @@ void  HumanPlayerStrategy::issueOrder() {
                     cout << "\nSuccess!\n";
                 }
             }
-
-            //Cards
-            bool flag = false;
-            while (!flag) {
-                cout << "\nChoose a card\n";
-                showCards();
-                int cardIndex;
-                cin >> cardIndex;
-                switch (handCard[cardIndex]->getType()) {
-                case bomb: {
-                    cout << "\nbomb";
-                    cout << "\nSelect a target";
-                    int tID;
-                    cin >> tID;
-                    Bomb* b = new Bomb(this, Map::worldMap.territories[tID - 1]->getTID());
-                    OrderList.push_back(*b);
-                    flag = true;
-                    break;
-                }
-                case reinforcement: {
-                    cout << "\nreinforcement";
-                    cout << "\nSelect a destination";
-                    int tID;
-                    cin >> tID;
-                    Deploy* d = new Deploy(this, Map::worldMap.territories[tID - 1]->getTID(), 5);
-                    OrderList.push_back(*d);
-                    flag = true;
-                    break;
-                }
-                case blockade: {
-                    cout << "\nblockade";
-                    cout << "\nSelect a target";
-                    int tID;
-                    cin >> tID;
-                    Blockade* b = new Blockade(this, Map::worldMap.territories[tID - 1]->getTID());
-                    OrderList.push_back(*b);
-                    flag = true;
-                    break;
-                }
-                case airlift: {
-                    cout << "\nairlift";
-                    cout << "\nSelect a destination";
-                    int tID;
-                    cin >> tID;
-                    cout << "\nHow many troops are being moved?";
-                    int num;
-                    cin >> num;
-                    cout << "\nWhere are they being taken from?";
-                    int tID2;
-                    cin >> tID2;
-                    Airlift* a = new Airlift(this, Map::worldMap.territories[tID2 - 1]->getTID(),
-                        Map::worldMap.territories[tID - 1]->getTID(), num);
-                    OrderList.push_back(*a);
-                    flag = true;
-                    break;
-                }
-                case diplomacy: {
-                    cout << "\ndiplomacy";
-                    cout << "\nSelect a Player";
-                    int pID2;
-                    cin >> pID2;
-                    Negotiate* n = new Negotiate(this, this->getPlayerByID(pID2));
-                    OrderList.push_back(*n);
-                    flag = true;
-                    break;
-                }
-                default: {
-                    cout << "invild input, try again!\n\n" << endl;
-
-                }
-                }
-            }
         };
 vector<Territory> HumanPlayerStrategy::toAttack(const string& s) {
-         vector<int> attacks = parseStringToInt(s);
-         for (auto i : attacks) {
+         vector<int> toAttacklist = parseStringToInt(s);
+         for (auto i : toAttacklist) {
 
              cout << "\nYou selected: " << Map::worldMap.territories[i - 1]->getTName();
 
@@ -173,8 +104,8 @@ vector<Territory> HumanPlayerStrategy::toAttack(const string& s) {
          return toAttacklist;
      };
 vector<Territory> HumanPlayerStrategy::todenfend(const string& s {
-         vector<int> defends = parseStringToInt(s);
-         for (auto i : defends) {
+         vector<int> todefendlist = parseStringToInt(s);
+         for (auto i : todefendlist) {
              todefendlist.push_back(i);
              cout << "\nYou selected: " << Map::worldMap.territories[i - 1]->getTName();
          }
@@ -263,77 +194,6 @@ void AggressivePlayerStrategy::issueOrder() {
         }
     }
 
-    //Cards
-    bool flag = false;
-    while (!flag) {
-        cout << "\nChoose a card\n";
-        showCards();
-        int cardIndex;
-        cin >> cardIndex;
-        switch (handCard[cardIndex]->getType()) {
-        case bomb: {
-            cout << "\nbomb";
-            cout << "\nSelect a target";
-            int tID;
-            cin >> tID;
-            Bomb* b = new Bomb(this, Map::worldMap.territories[tID - 1]->getTID());
-            OrderList.push_back(*b);
-            flag = true;
-            break;
-        }
-        case reinforcement: {
-            cout << "\nreinforcement";
-            cout << "\nSelect a destination";
-            int tID;
-            cin >> tID;
-            Deploy* d = new Deploy(this, Map::worldMap.territories[tID - 1]->getTID(), 5);
-            OrderList.push_back(*d);
-            flag = true;
-            break;
-        }
-        case blockade: {
-            cout << "\nblockade";
-            cout << "\nSelect a target";
-            int tID;
-            cin >> tID;
-            Blockade* b = new Blockade(this, Map::worldMap.territories[tID - 1]->getTID());
-            OrderList.push_back(*b);
-            flag = true;
-            break;
-        }
-        case airlift: {
-            cout << "\nairlift";
-            cout << "\nSelect a destination";
-            int tID;
-            cin >> tID;
-            cout << "\nHow many troops are being moved?";
-            int num;
-            cin >> num;
-            cout << "\nWhere are they being taken from?";
-            int tID2;
-            cin >> tID2;
-            Airlift* a = new Airlift(this, Map::worldMap.territories[tID2 - 1]->getTID(),
-                Map::worldMap.territories[tID - 1]->getTID(), num);
-            OrderList.push_back(*a);
-            flag = true;
-            break;
-        }
-        case diplomacy: {
-            cout << "\ndiplomacy";
-            cout << "\nSelect a Player";
-            int pID2;
-            cin >> pID2;
-            Negotiate* n = new Negotiate(this, this->getPlayerByID(pID2));
-            OrderList.push_back(*n);
-            flag = true;
-            break;
-        }
-        default: {
-            cout << "invild input, try again!\n\n" << endl;
-
-        }
-        }
-    }
 };
 vector<Territory> AggressivePlayerStrategy::toAttack() {
     vector <Territory> agrList;
@@ -362,12 +222,18 @@ vector<Territory> AggressivePlayerStrategy::toAttack() {
     return agrList;
 };
 vector<Territory> AggressivePlayerStrategy::todenfend(const string& s{
-         vector<int> defends = parseStringToInt(s);
-         for (auto i : defends) {
-             todefendlist.push_back(i);
-             cout << "\nYou selected: " << Map::worldMap.territories[i - 1]->getTName();
-         }
-         return todefendlist;
+         vector <Territory> dfdList;
+    vector<int> defends = parseStringToInt(s);
+    for (int i = 0; i < Map::worldMap.territories.size(); i++) {
+
+        string territoryPlayer = Map::worldMap.territories[i]->getTerritoryPlayer();
+        if (territoryPlayer == this->player) {
+            agrList.push_back(worldMap.territories[i])
+        }
+
+    }
+         return dfdList;
+         
     };
 
 
@@ -451,92 +317,25 @@ void BenevolentPlayerStrategy::issueOrder() {
         }
     }
 
-    //Cards
-    bool flag = false;
-    while (!flag) {
-        cout << "\nChoose a card\n";
-        showCards();
-        int cardIndex;
-        cin >> cardIndex;
-        switch (handCard[cardIndex]->getType()) {
-        case bomb: {
-            cout << "\nbomb";
-            cout << "\nSelect a target";
-            int tID;
-            cin >> tID;
-            Bomb* b = new Bomb(this, Map::worldMap.territories[tID - 1]->getTID());
-            OrderList.push_back(*b);
-            flag = true;
-            break;
-        }
-        case reinforcement: {
-            cout << "\nreinforcement";
-            cout << "\nSelect a destination";
-            int tID;
-            cin >> tID;
-            Deploy* d = new Deploy(this, Map::worldMap.territories[tID - 1]->getTID(), 5);
-            OrderList.push_back(*d);
-            flag = true;
-            break;
-        }
-        case blockade: {
-            cout << "\nblockade";
-            cout << "\nSelect a target";
-            int tID;
-            cin >> tID;
-            Blockade* b = new Blockade(this, Map::worldMap.territories[tID - 1]->getTID());
-            OrderList.push_back(*b);
-            flag = true;
-            break;
-        }
-        case airlift: {
-            cout << "\nairlift";
-            cout << "\nSelect a destination";
-            int tID;
-            cin >> tID;
-            cout << "\nHow many troops are being moved?";
-            int num;
-            cin >> num;
-            cout << "\nWhere are they being taken from?";
-            int tID2;
-            cin >> tID2;
-            Airlift* a = new Airlift(this, Map::worldMap.territories[tID2 - 1]->getTID(),
-                Map::worldMap.territories[tID - 1]->getTID(), num);
-            OrderList.push_back(*a);
-            flag = true;
-            break;
-        }
-        case diplomacy: {
-            cout << "\ndiplomacy";
-            cout << "\nSelect a Player";
-            int pID2;
-            cin >> pID2;
-            Negotiate* n = new Negotiate(this, this->getPlayerByID(pID2));
-            OrderList.push_back(*n);
-            flag = true;
-            break;
-        }
-        default: {
-            cout << "invild input, try again!\n\n" << endl;
 
-        }
-        }
-    }
 };
 vector<Territory> BenevolentPlayerStrategy::toAttack(const string& s) {
+    vector <Territory> agrList;
     vector<int> attacks = parseStringToInt(s);
-    for (auto i : attacks) {
+    for (int i = 0; i < Map::worldMap.territories.size(); i++) {
 
-        cout << "\nYou selected: " << Map::worldMap.territories[i - 1]->getTName();
+        string territoryPlayer = Map::worldMap.territories[i]->getTerritoryPlayer();
+        if (territoryPlayer == this->player) {
+            agrList.push_back(worldMap.territories[i])
+        }
 
-        toAttacklist.push_back(i);
     }
 
-    return toAttacklist;
+    return agrList;
 };
 vector<Territory> BenevolentPlayerStrategy::todenfend(const string& s{
         vector <Territory> dfdList;
-    vector<int> attacks = parseStringToInt(s);
+    vector<int> defends = parseStringToInt(s);
     for (int i = 0; i < Map::worldMap.territories.size(); i++) {
 
         string territoryPlayer = Map::worldMap.territories[i]->getTerritoryPlayer();
@@ -569,24 +368,32 @@ vector<Territory> BenevolentPlayerStrategy::todenfend(const string& s{
 void NeutralPlayerStrategy::issueOrder() {
     cout << "\nNO ISSUE ORDER: \n" << endl;
 };
-vector<Territory> NeutralPlayerStrategy::toAttack(const string& s) {
-    vector<int> attacks = parseStringToInt(s);
-    for (auto i : attacks) {
+vector<Territory> BenevolentPlayerStrategy::toAttack(const string & s) {
+        vector <Territory> agrList;
+        vector<int> attacks = parseStringToInt(s);
+        for (int i = 0; i < Map::worldMap.territories.size(); i++) {
 
-        cout << "\nYou selected: " << Map::worldMap.territories[i - 1]->getTName();
+            string territoryPlayer = Map::worldMap.territories[i]->getTerritoryPlayer();
+            if (territoryPlayer == this->player) {
+                agrList.push_back(worldMap.territories[i])
+            }
 
-        toAttacklist.push_back(i);
-    }
+        }
 
-    return toAttacklist;
+        return agrList;
 };
 vector<Territory> NeutralPlayerStrategy::todenfend(const string& s{
-         vector<int> defends = parseStringToInt(s);
-         for (auto i : defends) {
-             todefendlist.push_back(i);
-             cout << "\nYou selected: " << Map::worldMap.territories[i - 1]->getTName();
-         }
-         return todefendlist;
+    vector <Territory> dfdList;
+    vector<int> defends = parseStringToInt(s);
+    for (int i = 0; i < Map::worldMap.territories.size(); i++) {
+
+        string territoryPlayer = Map::worldMap.territories[i]->getTerritoryPlayer();
+        if (territoryPlayer == this->player) {
+            agrList.push_back(worldMap.territories[i])
+        }
+
+    }
+         return dfdList;
     };
 
 // computer player which always try to attack nearby territories -g
@@ -669,103 +476,32 @@ void CheaterPlayerStrategy::issueOrder() {
         }
     }
 
-    //Cards
-    bool flag = false;
-    while (!flag) {
-        cout << "\nChoose a card\n";
-        showCards();
-        int cardIndex;
-        cin >> cardIndex;
-        switch (handCard[cardIndex]->getType()) {
-        case bomb: {
-            cout << "\nbomb";
-            cout << "\nSelect a target";
-            int tID;
-            cin >> tID;
-            Bomb* b = new Bomb(this, Map::worldMap.territories[tID - 1]->getTID());
-            OrderList.push_back(*b);
-            flag = true;
-            break;
-        }
-        case reinforcement: {
-            cout << "\nreinforcement";
-            cout << "\nSelect a destination";
-            int tID;
-            cin >> tID;
-            Deploy* d = new Deploy(this, Map::worldMap.territories[tID - 1]->getTID(), 5);
-            OrderList.push_back(*d);
-            flag = true;
-            break;
-        }
-        case blockade: {
-            cout << "\nblockade";
-            cout << "\nSelect a target";
-            int tID;
-            cin >> tID;
-            Blockade* b = new Blockade(this, Map::worldMap.territories[tID - 1]->getTID());
-            OrderList.push_back(*b);
-            flag = true;
-            break;
-        }
-        case airlift: {
-            cout << "\nairlift";
-            cout << "\nSelect a destination";
-            int tID;
-            cin >> tID;
-            cout << "\nHow many troops are being moved?";
-            int num;
-            cin >> num;
-            cout << "\nWhere are they being taken from?";
-            int tID2;
-            cin >> tID2;
-            Airlift* a = new Airlift(this, Map::worldMap.territories[tID2 - 1]->getTID(),
-                Map::worldMap.territories[tID - 1]->getTID(), num);
-            OrderList.push_back(*a);
-            flag = true;
-            break;
-        }
-        case diplomacy: {
-            cout << "\ndiplomacy";
-            cout << "\nSelect a Player";
-            int pID2;
-            cin >> pID2;
-            Negotiate* n = new Negotiate(this, this->getPlayerByID(pID2));
-            OrderList.push_back(*n);
-            flag = true;
-            break;
-        }
-        default: {
-            cout << "invild input, try again!\n\n" << endl;
-
-        }
-        }
-    }
+    
 };
 vector<Territory> CheaterPlayerStrategy::toAttack(const string& s) {
+    vector <Territory> agrList;
     vector<int> attacks = parseStringToInt(s);
-   
+    for (int i = 0; i < Map::worldMap.territories.size(); i++) {
 
-        for (int i = 0; i < Map::worldMap.territories.size(); i++) {
-            string territoryPlayer = Map::worldMap.territories[i]->getTerritoryPlayer();
-            string territoryName = Map::worldMap.territories[i]->getTName();
-            int id = Map::worldMap.territories[i]->tID;
-            if (territoryPlayer != (playerName)) {
-                cout << "Territory: " << territoryName << ", owned by " << territoryPlayer << " id: " << id << endl;
-            }
-
-            toAttacklist.push_back(id);
+        string territoryPlayer = Map::worldMap.territories[i]->getTerritoryPlayer();
+        if (territoryPlayer == this->player) {
+            agrList.push_back(worldMap.territories[i])
         }
 
-        
-    
+    }
 
-    return toAttacklist;
+    return agrList;
 };
 vector<Territory> CheaterPlayerStrategy::todenfend(const string& s{
-         vector<int> defends = parseStringToInt(s);
-         for (auto i : defends) {
-             todefendlist.push_back(i);
-             cout << "\nYou selected: " << Map::worldMap.territories[i - 1]->getTName();
-         }
-         return todefendlist;
+    vector <Territory> dfdList;
+    vector<int> defends = parseStringToInt(s);
+    for (int i = 0; i < Map::worldMap.territories.size(); i++) {
+
+        string territoryPlayer = Map::worldMap.territories[i]->getTerritoryPlayer();
+        if (territoryPlayer == this->player) {
+            agrList.push_back(worldMap.territories[i])
+        }
+
+    }
+         return dfdList;
     };
